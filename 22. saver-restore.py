f578cd4d7294e42d7bfd0089b758ae3b9e354445
dovletov <kurban.dovletov@gmail.com>
2018-08-14 15:09:42 +0200

    Add tensor names. Needed for restoring

--- 22. saver-restore.py
+++ 22. saver-restore.py
@@ -14,9 +14,9 @@ config.gpu_options.allow_growth = True
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # placeholders
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
-keep_prob = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32, shape=[None, 784], name="input_x")
+y_ = tf.placeholder(tf.float32, shape=[None, 10], name="input_y_")
+keep_prob = tf.placeholder(tf.float32, name="input_keep_prob")
 
 # model
 def cnn(x, keep_prob):
@@ -76,12 +76,14 @@ global_step = tf.Variable(0, name='global_step', trainable=False)
 
 # cross-entropy loss function
 loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv),
+    name="op_loss")
 train_step = tf.train.AdamOptimizer(1e-4).minimize(loss, global_step=global_step)
 
 # evaluation
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32),
+  name="op_accuracy")
 
 # summaries
 x_image = tf.summary.image('image', tf.reshape(x, [-1, 28, 28, 1]), 10)
@@ -120,8 +122,8 @@ with tf.Session(config=config) as sess:
             vl_loss, vl_acc = sess.run([loss, accuracy], feed_dict=feed_test)
             print('Step  %d, validation accuracy %g' % (step_id,vl_acc))
 
-            writer_1.add_summary(sess.run(summaries, feed_dict=feed_train), step_id)
-            writer_2.add_summary(sess.run(summaries, feed_dict=feed_test), step_id)
+            writer_1.add_summary(sess.run(summaries, feed_dict=feed_train), global_step.eval())
+            writer_2.add_summary(sess.run(summaries, feed_dict=feed_test), global_step.eval())
 
             saver.save(sess, ckpt_dir + '21/model.ckpt', global_step=global_step.eval())
 
